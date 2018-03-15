4c72e69fde3a5e85fc8eb34fdbeb19a23d69ed10
Emil Alasgarov <debiankondor95@hotmail.com>
2018-03-15 22:36:56 +0400

    Added panel, on which we can draw. Added clear button

--- shape-recognizer/Form1.Designer.cs
+++ shape-recognizer/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.graphPanel = new System.Windows.Forms.Panel();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // graphPanel
+            // 
+            this.graphPanel.BackColor = System.Drawing.Color.White;
+            this.graphPanel.Location = new System.Drawing.Point(12, 12);
+            this.graphPanel.Name = "graphPanel";
+            this.graphPanel.Size = new System.Drawing.Size(373, 293);
+            this.graphPanel.TabIndex = 0;
+            this.graphPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.graphPanel_MouseDown);
+            this.graphPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.graphPanel_MouseMove);
+            this.graphPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.graphPanel_MouseUp);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(155, 311);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(392, 342);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.graphPanel);
+            this.Name = "Form1";
+            this.Text = "Shape Recognizer";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel graphPanel;
+        private System.Windows.Forms.Button buttonClear;
     }
 }
 
--- shape-recognizer/Form1.cs
+++ shape-recognizer/Form1.cs
@@ -12,9 +12,50 @@ namespace shape_recognizer
 {
     public partial class Form1 : Form
     {
+        private Graphics graphObj;
+        private Point _lastLoc;
+        bool mouseIsDown = false;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void graphPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseIsDown = true;
+                graphObj = this.graphPanel.CreateGraphics();
+                _lastLoc = e.Location;
+            }
+        }
+
+        private void graphPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseIsDown)
+            {                
+                paintCurrentPosition(e.Location, 2, Color.Blue);
+            }
+        }
+        private void paintCurrentPosition(Point loc, int thickness, Color colorPen)
+        {           
+            graphObj.DrawLine(new Pen(Color.Black), _lastLoc, loc);
+            _lastLoc = loc;
+        }
+
+        private void graphPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseIsDown = false;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            if(graphObj != null)
+            {
+                graphObj.Clear(Color.White);
+            }
+        }
     }
 }
+
