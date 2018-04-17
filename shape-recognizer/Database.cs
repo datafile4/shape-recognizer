83d67e73057657b6f40945db1096b7c9b70d4d2c
Emil Alasgarov <debianmik@gmail.com>
2018-04-17 17:53:27 +0400

    Minor changes

--- shape-recognizer/Database.cs
+++ shape-recognizer/Database.cs
@@ -6,15 +6,26 @@ using System.Threading.Tasks;
 
 namespace shape_recognizer
 {
+    public struct Relations
+    {
+        public double LenPch;
+        public double Pch2Ach;
+        public double AltAch;
+        public double PchPer;
+        public double AchAerAlt;
+    }
+
     class Database
     {
         public List<Polygon2D> Shapes { get; private set; }
         public List<string> Classes { get; private set; }
+        public List<Relations> Features { get; private set; }
 
         public Database()
         {
             Classes = new List<string>();
             Shapes = new List<Polygon2D>();
+            Features = new List<Relations>();
         }
     }
 }
--- shape-recognizer/Polygon2d.cs
+++ shape-recognizer/Polygon2d.cs
@@ -5,23 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace shape_recognizer
-{
-    public struct Relations
-    {
-        public double LenPch;
-        public double Pch2Ach;
-        public double AltAch;
-        public double PchPer;
-        public double AchAerAlt;
-    }
+{    
     class Polygon2D
     {
-        private List<Point> _points { get; set; }
+        public List<Point> Points { get; set; }
         public Polygon2D(List<Point> points)
         {
-            _points = points;
+            this.Points = points;
         }
     }
 }
