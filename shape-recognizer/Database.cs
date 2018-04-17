feb75cb32ad5c895169ccf488eccfeaae4a8ed17
Emil Alasgarov <debiankondor95@hotmail.com>
2018-04-17 07:26:29 +0400

    minor changes

--- shape-recognizer/Database.cs
+++ shape-recognizer/Database.cs
@@ -8,5 +8,13 @@ namespace shape_recognizer
 {
     class Database
     {
+        public List<Polygon2D> Shapes { get; private set; }
+        public List<string> Classes { get; private set; }
+
+        public Database()
+        {
+            Classes = new List<string>();
+            Shapes = new List<Polygon2D>();
+        }
     }
 }
--- shape-recognizer/Polygon2d.cs
+++ shape-recognizer/Polygon2d.cs
@@ -18,11 +18,9 @@ namespace shape_recognizer
     }
     class Polygon2D
     {
-        private List<Point> _points { get; set; }       
-        //private Relations _relations { get; set; }
-        public Polygon2D(List<Point> points, Relations relations)
+        private List<Point> _points { get; set; }
+        public Polygon2D(List<Point> points)
         {
-            //_relations = relations;
             _points = points;
         }
     }
